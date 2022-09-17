0c82e6ba50ab688ef59f67eed0ad497de1ce9a6d
Josel Catalan <catalanjosel123@gmail.com>
2022-09-17 20:12:11 +0800

    fix: bottom page on small screens

--- src/components/UI/Container.jsx
+++ src/components/UI/Container.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Container({ children }) {
-  return <div className="mx-auto max-w-4xl p-4">{children}</div>
+  return <div className="mx-auto max-w-4xl p-4 my-1">{children}</div>
 }
 
 export default Container
