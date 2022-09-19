620db8faea5487b53d367142e9de585924d7b485
Josel Catalan <catalanjosel123@gmail.com>
2022-09-19 16:16:15 +0800

    implemented portals

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,32 +1,52 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Card from './Card'
 import Button from './Button'
 
-function ErrorModal({ title, message, closeModal }) {
+function Backdrop({ children, closeModal }) {
   return (
     <div
       onClick={closeModal}
       className="p-4 fixed top-0 left-0 w-screen h-screen bg-slate-500/80 grid place-items-center animate-fade-in"
     >
-      <Card
-        onClick={(e) => e.stopPropagation()}
-        className="w-4/5 max-w-md bg-white"
-      >
-        <div className="flex flex-col gap-4 items-center">
-          <header>
-            <h2 className="font-bold text-3xl">{title}</h2>
-          </header>
-          <div className="max-h-96 overflow-y-auto text-center">
-            <p>{message}</p>
-          </div>
-          <footer>
-            <Button onClick={closeModal} primary>
-              Okay
-            </Button>
-          </footer>
-        </div>
-      </Card>
+      {children}
     </div>
+  )
+}
+
+function Modal({ title, message, closeModal }) {
+  return (
+    <Card
+      onClick={(e) => e.stopPropagation()}
+      className="w-4/5 max-w-md bg-white"
+    >
+      <div className="flex flex-col gap-4 items-center">
+        <header>
+          <h2 className="font-bold text-3xl">{title}</h2>
+        </header>
+        <div className="max-h-96 overflow-y-auto text-center">
+          <p>{message}</p>
+        </div>
+        <footer>
+          <Button onClick={closeModal} primary>
+            Okay
+          </Button>
+        </footer>
+      </div>
+    </Card>
+  )
+}
+
+function ErrorModal({ title, message, closeModal }) {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop closeModal={closeModal}>
+          <Modal title={title} message={message} closeModal={closeModal} />
+        </Backdrop>,
+        document.getElementById('backdrop-root')
+      )}
+    </>
   )
 }
 
